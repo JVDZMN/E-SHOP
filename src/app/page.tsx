f06171f8cf4a4d72d4fb0723be4cde5b3c4fe7d8
Javad <jvdzmn@gmail.com>
2025-03-30 17:11:48 +0200

    adding theming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="text-red-500">HELOO</div>
+    <div className="p-5">
+      <div className="w-full flex justify-end" ><ThemeToggle /></div>
+      <h1 className="text-blue-500"> welcome to my shop</h1>
+      <Button>Click here!</Button>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'tailwindcss'
 import animate from 'tailwindcss-animate'
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
